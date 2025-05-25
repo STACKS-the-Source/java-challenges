22e9f2ef049446aa18c8104527b29cb2b30cea7e
thecyberelder <saadlegendcontact78@gmail.com>
2025-05-25 05:44:04 +0300

    Create App.java

---
+++ App.java
@@ -0,0 +1,7 @@
+// This file named App.java
+// create here public class for file "App" :
+
+
+// create here a variable named "name" with string type , Notice : "String type"
+
+//create here output for this variable
