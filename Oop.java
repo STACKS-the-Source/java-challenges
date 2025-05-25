3718c31ed1637988d1d632bf9ecd1412ef012234
thecyberelder <saadlegendcontact78@gmail.com>
2025-05-25 05:45:35 +0300

    Create Oop.java

---
+++ Oop.java
@@ -0,0 +1,15 @@
+public class Oop {
+// create variables named (encapsulation , inheritance , polymorphism)
+
+
+// create counstructor
+
+
+// create getters-setters
+
+
+
+// This file for configure "Main.java" as a driver class.
+
+
+}
